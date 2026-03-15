863f23cb7389374f957d0d70167898973a843f6e
DevisedWeb <contact@devisedweb.com>
2026-03-15 17:10:42 +0500

    Started working on JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const startGame = document.getElementById("start-game");
+const cardsEl = document.getElementById("cards-el");
+const sumEl = document.getElementById("sum-el");
+const messageEl = document.getElementById("message-el");
+const newCard = document.getElementById("new-card");
+const playerEl = document.getElementById("player-el");
